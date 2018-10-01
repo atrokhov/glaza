8b9d2ca135b4c5d630eca050b8b6024ed2349b92
Arthur Atrokhov <aatrokhov@gmail.com>
2018-10-01 16:42:53 +0600

    Parser and tests are ready

--- Instagram/parser.py
+++ Instagram/parser.py
@@ -12,39 +12,38 @@ instagram_users_data = db.instagram_users_data
 def get_user_data(username):
     url = 'https://www.instagram.com/' + username
     db_check = instagram_users_data.find_one({"username": username})
+    r = requests.get(url)
+    if r:
+        soup = BeautifulSoup(r.content, 'html.parser')
+        script = soup.find('script', text=re.compile('window._sharedData'))
+        json_text = re.search(r'^\s*window\._sharedData\s*=\s*({.*?})\s*;\s*$', script.string, flags=re.DOTALL | re.MULTILINE).group(1)
+        data = json.loads(json_text)['entry_data']['ProfilePage'][0]['graphql']['user']
+
+        media_urls = []
+        follows = data['edge_follow']['count']
+        followed_by = data['edge_followed_by']['count']
+        media_count = data['edge_owner_to_timeline_media']['count']
+        media = data['edge_owner_to_timeline_media']['edges']
+
+        if media:
+            for i in range(0, 3):
+                media_urls.append("https://www.instagram.com/p/" + str(media[i]['node']['shortcode']))
+
+        user_data = {
+            'username':username,
+            'media_urls': media_urls,
+            'follows': follows,
+            'followed_by': followed_by,
+            'media_count': media_count
+        }
+
+        return user_data
+
+
+def user_info(username, db=instagram_users_data):
+    db_check = instagram_users_data.find_one({"username": username})
     if not db_check:
-        r = requests.get(url)
-        if r:
-            soup = BeautifulSoup(r.content, 'html.parser')
-            script = soup.find('script', text=re.compile('window._sharedData'))
-            json_text = re.search(r'^\s*window\._sharedData\s*=\s*({.*?})\s*;\s*$', script.string, flags=re.DOTALL | re.MULTILINE).group(1)
-            data = json.loads(json_text)['entry_data']['ProfilePage'][0]['graphql']['user']
-
-            media_urls = []
-            follows = data['edge_follow']['count']
-            followed_by = data['edge_followed_by']['count']
-            media_count = data['edge_owner_to_timeline_media']['count']
-            media = data['edge_owner_to_timeline_media']['edges']
-
-            if media:
-                for i in range(0, 3):
-                    media_urls.append("https://www.instagram.com/p/" + str(media[i]['node']['shortcode']))
-
-            user_data = {
-                'username':username,
-                'media_urls': media_urls,
-                'follows': follows,
-                'followed_by': followed_by,
-                'media_count': media_count
-            }
-
-            instagram_users_data.insert_one(user_data)
-
-            return user_data
-        else:
-            return db_check
+        instagram_users_data.insert_one(get_user_data(username))
+        return db_check
     else:
         return db_check
-
-instagram_users_data.drop()
-print get_user_data('_yanita')
--- Instagram/test.py
+++ Instagram/test.py
@@ -3,26 +3,38 @@
 import unittest
 from parser import * 
 
+client = MongoClient('localhost', 27017)
+db = client.glaza
+
+test_instagram_users_data = db.test_instagram_users_data
+
 class TestParser(unittest.TestCase):
 
     def test_parser_with_my_user(self):
-        self.assertEqual(get_user_data("helloworld7743"), instagram_users_data.find_one({'username': 'helloworld7743'}))
+        result = {'username': 'helloworld7743', 'follows': 4, 'media_urls': ['https://www.instagram.com/p/BoLhd7HBtI4', 'https://www.instagram.com/p/BoLhcYHhnLg', 'https://www.instagram.com/p/BoLhZkhh72n'], 'media_count': 5, 'followed_by': 0}
+        self.assertEqual(get_user_data("helloworld7743"), result)
 
     def test_parser_with_another_user(self):
-        self.assertEqual(get_user_data("dawda"), instagram_users_data.find_one({'username': 'dawda'}))
+        result = {'username': 'dawda', 'follows': 1395, 'media_urls': ['https://www.instagram.com/p/BoJDDKMAc4r', 'https://www.instagram.com/p/BnOUl5Fggan', 'https://www.instagram.com/p/Bm74PzaAKUI'], 'media_count': 85, 'followed_by': 2791}
+        self.assertEqual(get_user_data("dawda"), result)
 
     def test_parser_with_not_exicting_user(self):
-        self.assertEqual(get_user_data("hagfjgafgafg"), None)
+        self.assertIsNone(get_user_data("hagfjgafgafg"))
 
+    def test_inserting_to_db(self):
+        test_instagram_users_data.drop()
+        test_instagram_users_data.insert_one(user_info('helloworld7743', db=test_instagram_users_data))
+        self.assertEqual(test_instagram_users_data.count(), 1)
+        
     def test_parser_with_one_user_two_times(self):
-        get_user_data("helloworld7743")
-        get_user_data("helloworld7743")
+        test_instagram_users_data.drop()
+        user_info("helloworld7743", db=test_instagram_users_data)
+        user_info("helloworld7743", db=test_instagram_users_data)
         self.assertEqual(instagram_users_data.find({'username': 'helloworld7743'}).count(), 1)
 
-    def test_parser_with_one_user(self):
-        get_user_data("_yanita")
-        result = {u'username': u'_yanita', u'follows': 2, u'followed_by': 5, u'media_count': 0, u'media_urls': []}
-        self.assertIn(result, instagram_users_data.find({'username': '_yanita'}))
+    def test_parser_for_correct_inserting_to_db(self):
+        test_instagram_users_data.drop()
+        self.assertEqual(user_info("dawda", db=test_instagram_users_data), instagram_users_data.find_one({'username': 'dawda'}))
 
 
 if __name__ == '__main__':
