b1168e85754c2ffdb6513dc0ec6e1601caa198be
Arthur Atrokhov <aatrokhov@gmail.com>
2018-10-01 15:34:20 +0600

    Rewrite code to html parser

--- Instagram/parser.py
+++ Instagram/parser.py
@@ -1,5 +1,8 @@
 import requests
+import re
+from bs4 import BeautifulSoup
 from pymongo import MongoClient
+import json
 
 client = MongoClient('localhost', 27017)
 db = client.glaza
@@ -7,20 +10,22 @@ db = client.glaza
 instagram_users_data = db.instagram_users_data
 
 def get_user_data(username):
-    url = "https://apinsta.herokuapp.com/u/" + username
+    url = 'https://www.instagram.com/' + username
     db_check = instagram_users_data.find_one({"username": username})
     if not db_check:
-        soup = BeautifulSoup(requests.get(url).content, 'html.parser')
-        script = soup.find('script', text=re.compile('window._sharedData'))
-        json_text = re.search(r'^\s*window\._sharedData\s*=\s*({.*?})\s*;\s*$',
-                      script.string, flags=re.DOTALL | re.MULTILINE).group(1)
-        data = json.loads(json_text)['entry_data']['ProfilePage'][0]['graphql']['user']
+        r = requests.get(url)
         if r:
+            soup = BeautifulSoup(r.content, 'html.parser')
+            script = soup.find('script', text=re.compile('window._sharedData'))
+            json_text = re.search(r'^\s*window\._sharedData\s*=\s*({.*?})\s*;\s*$', script.string, flags=re.DOTALL | re.MULTILINE).group(1)
+            data = json.loads(json_text)['entry_data']['ProfilePage'][0]['graphql']['user']
+
             media_urls = []
             follows = data['edge_follow']['count']
             followed_by = data['edge_followed_by']['count']
             media_count = data['edge_owner_to_timeline_media']['count']
             media = data['edge_owner_to_timeline_media']['edges']
+
             if media:
                 for i in range(0, 3):
                     media_urls.append("https://www.instagram.com/p/" + str(media[i]['node']['shortcode']))
@@ -35,8 +40,11 @@ def get_user_data(username):
 
             instagram_users_data.insert_one(user_data)
 
-            return db_check
+            return user_data
         else:
             return db_check
     else:
         return db_check
+
+instagram_users_data.drop()
+print get_user_data('_yanita')
--- Instagram/test.py
+++ Instagram/test.py
@@ -19,5 +19,11 @@ class TestParser(unittest.TestCase):
         get_user_data("helloworld7743")
         self.assertEqual(instagram_users_data.find({'username': 'helloworld7743'}).count(), 1)
 
+    def test_parser_with_one_user(self):
+        get_user_data("_yanita")
+        result = {u'username': u'_yanita', u'follows': 2, u'followed_by': 5, u'media_count': 0, u'media_urls': []}
+        self.assertIn(result, instagram_users_data.find({'username': '_yanita'}))
+
+
 if __name__ == '__main__':
     unittest.main()
